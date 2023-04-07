be508a246ef1875fe65e1f316dffc78714346387
Pghosh7994 <priyankapinki@gmail.com>
2023-04-07 19:22:36 +0600

    convert image in a text

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import pytesseract
 
 # Initialize webcam
 cap = cv2.VideoCapture(0)
@@ -28,3 +29,12 @@ cv2.imshow('Grayscale Image', gray_image)
 # cv2.imshow("Captured Image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Apply thresholding to remove noise
+thresh = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+# Use pytesseract to convert the image to text
+text = pytesseract.image_to_string(thresh)
+
+# Print the text
+print(text)
