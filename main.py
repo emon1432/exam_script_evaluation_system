c577215fe688279ace279ed66614b7c681c6a61d
emon1432 <ovirajemon143@gmail.com>
2023-04-09 23:53:55 +0600

    image to text convert start

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import cv2
+import pytesseract
 
 # Initialize webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('http://192.168.1.155:4747/video')
 
 # Capture an image when space key is pressed
 while True:
     ret, frame = cap.read()
     cv2.imshow("Image Capture", frame)
     if cv2.waitKey(1) == ord(' '):
-        cv2.imwrite("captured_image.jpg", frame)
+        cv2.imwrite("captured_image1.jpg", frame)
         break
 
 # Release webcam and close all windows
 cap.release()
 cv2.destroyAllWindows()
 
-# open the captured image
-img = cv2.imread("captured_image.jpg")
+# Read the image
+img = cv2.imread('captured_image1.jpg')
 
 # Convert image to grayscale
-gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Display original and grayscale images
-cv2.imshow('Original Image', frame)
-cv2.imshow('Grayscale Image', gray_image)
+# Pre-process the image to remove noise and smooth it
+gray = cv2.medianBlur(gray, 3)
+gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-# cv2.imshow("Captured Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Perform OCR using Tesseract
+text = pytesseract.image_to_string(gray, lang='eng')
+
+# Print the extracted text
+print(text)
